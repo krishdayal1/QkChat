c949326f3290a6a8b6c66e214ea30707d83da4aa
Krish Dayal <krishdayal2006@gmail.com>
2026-08-09 19:07:19 +0530

    Add astra only for the auth user and solved bugs like astra duplicate panel rendering, fixed mobile and dekstop astra behavior and improved architecture

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { LogOut, MessageSquare, Settings, User } from 'lucide-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import astra from "../assets/astra.png";
-import { useAstraStore } from '../store/useAstraStore';
+import { useAstraStore } from "../store/useAstraStore";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -17,48 +17,54 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
               <h1 className="text-lg font-bold">QkChat</h1>
             </Link>
           </div>
-          
-          <div className="flex items-center gap-2">
-            <button
-              onClick={openAstra}
-              disabled={isAstraOpen}
-              className="btn btn-sm gap-2"
-              >
-              <img
-                src={astra}
-                alt="Astra"
-                className="w-9 h-9 rounded-full object-cover"
-              />
 
-              <span className='hidden sm:inline'>
-                Astra
-              </span>
-            </button>
-            <Link
-              to={"/settings"}
-              className={`btn btn-sm transition-colors`}
-              >
-                <Settings className="w-4 h-4"/>
-                <span className="hidden sm:inline">Settings</span>
+          <div className="flex items-center gap-2">
+            {authUser && (
+              <>
+                <button
+                  onClick={openAstra}
+                  disabled={isAstraOpen}
+                  className="btn btn-sm gap-2"
+                >
+                  <img
+                    src={astra}
+                    alt="Astra"
+                    className="w-9 h-9 rounded-full object-cover"
+                  />
+
+                  <span className="hidden sm:inline">Astra</span>
+                </button>
+              </>
+            )}
+            <Link to={"/settings"} className={`btn btn-sm transition-colors`}>
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
             </Link>
 
             {authUser && (
               <>
                 <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-                  <User className='size-5' />
-                  <span className='hidden sm:inline'>Profile</span>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button type="button" className='flex gap-2 items-center' onClick={logout}>
+                <button
+                  type="button"
+                  className="flex gap-2 items-center"
+                  onClick={logout}
+                >
                   <LogOut className="size-5" />
-                  <span className='hidden sm:inline'>Logout</span>
+                  <span className="hidden sm:inline">Logout</span>
                 </button>
               </>
             )}
@@ -66,7 +72,7 @@ const Navbar = () => {
         </div>
       </div>
     </header>
-  ) 
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
